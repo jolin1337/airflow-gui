823f26dd86b1927ad3cfffadaab144021fb446de
Johannes Linden <johannes@godesity.se>
2021-03-27 16:30:50 +0100

    Ignore airflow warning logs to reduce clutter in STD

--- api/log.py
+++ api/log.py
@@ -1,10 +1,30 @@
 import logging
+import warnings
+import logging
+# NEeds to be here to configure airflow logging first
+from airflow import logging_config
 
-logging.basicConfig(
+log_config = dict(
     format="%(asctime)s %(levelname)-6s %(name)-25s %(funcName)-20s %(message)s",
-    level=logging.INFO,
+    level=logging.CRITICAL,
     datefmt="%Y-%m-%dT%H:%M:%S",
 )
-logging.getLogger("matplotlib").setLevel(logging.WARNING)
+
+
+class NoParsingFilter(logging.Filter):
+    def filter(self, record):
+        return not record.getMessage().startswith('BMB')
+
+
+for name in logging.Logger.manager.loggerDict.keys():
+    log = logging.getLogger(name)
+    log.setLevel(logging.CRITICAL)
+    log.addFilter(NoParsingFilter())
+
+
+warnings.filterwarnings("ignore")
+warnings.simplefilter("ignore")
+
+logging.basicConfig(**log_config)
 
 logger = logging
